dd1a79b3f3dcb24379c04e3a3d042fa9291ba36f
Vladimir-sas <57978034+Vladimir-sas@users.noreply.github.com>
2021-01-26 19:16:11 +0700

    Add files via upload

--- logging.py
+++ logging.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import sqlite3
+from tkinter import messagebox
+
+
 
 conn = sqlite3.connect('configs')
 cur = conn.cursor()
@@ -45,7 +48,7 @@ def reg_win(): # окно регистрации
         to_db = (login_ent.get(),password_ent.get())
         print(f'Логин:{login_ent.get()}',f' Пароль:{password_ent.get()}',f' Имя:{nick_ent.get()}' ,sep = " | ")
 
-        cur.execute('INSERT INTO ent_conf(login, password) VALUES (?,?)',to_db)
+
 
         cur.execute('SELECT login,password FROM ent_conf')
         rows = cur.fetchall()
@@ -54,6 +57,11 @@ def reg_win(): # окно регистрации
         for row in rows:
             print(row)
 
+        if to_db == row:
+            messagebox.showinfo("бабл квас", "Логин и пароль уже заняты")
+        else:
+            cur.execute('INSERT INTO ent_conf(login, password) VALUES (?,?)', to_db)
+            messagebox.showinfo("бабл квас", "Регистрация прошла успешно")
         # cur.execute('DELETE from ent_conf')
 
 
@@ -69,9 +77,48 @@ def enter_win():#окно входа
     enter_window.geometry("200x200")
     enter_window.title("Вкид")
 
+    login_var1 = StringVar()
+    password_var1 = StringVar()
+    nick_var1 = StringVar()
+
+    login_label1 = Label(enter_window, text="Логин:").place(x=10, y=50)
+    password_label1 = Label(enter_window, text="Пароль:").place(x=10, y=80)
+    password_label1 = Label(enter_window, text="Имя:").place(x=10, y=20)
+
+    login_ent1 = Entry(enter_window, textvariable=login_var1)
+    login_ent1.place(x=70, y=50)
+
+    password_ent1 = Entry(enter_window, textvariable=password_var1)
+    password_ent1.place(x=70, y=80)
+
+    nick_ent1 = Entry(enter_window, textvariable=nick_var1)
+    nick_ent1.place(x=70, y=20)
+
+    def check():
+        to_db = (login_ent1.get(), password_ent1.get())
+        cur.execute('SELECT login,password FROM ent_conf')
+
+        rows1 = cur.fetchall()
+
+        for row1 in rows1:
+            pass
+
+        if to_db == row1:
+            messagebox.showinfo("бабл квас","Вход оформлен")
+        else:
+            messagebox.showinfo("бабл квас", "Такого пользователя нет")
+
+        conn.commit()
+
+
+
+    ent_btn = Button(enter_window, text="Войти", background="green", foreground="black",
+                    activebackground="red", command=check,width=20).place(x=30, y=120)
+
 reg_btn = Button(root, text="Регистрация", width=20, background="green", foreground="black",activebackground="red",command = reg_win).place(x=130, y=200)
 
 enter_btn = Button(root, text="Вход", width=20, background="green", foreground="black", activebackground="red",command = enter_win).place(x=130, y=230)
+
 
 
 
